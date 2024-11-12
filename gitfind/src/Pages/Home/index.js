0c074bab6274d0d7300ecadc97c1a2cd44bd7363
Isaac Mikhail <isaac_mikhail@hotmail.com>
2024-11-12 13:11:29 -0300

    commit com erro 5:0-57

--- gitfind/src/Pages/Home/index.js
+++ gitfind/src/Pages/Home/index.js
@@ -1,8 +1,18 @@
 
+import { Header } from "../../Compenents/Header";
+import {background} from "./src/Assets/background.png";
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <div className="content">
+        <img src={background} className= "background" alt="background app"></img>
+        <div className="info">
+          <div>
+            <input name="user" placeholder="@username"></input></div>
+            <button>Search</button>
+        </div>
+      </div>
     </div>
   );
 }
